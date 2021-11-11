4c83db42dc542463a4ec86ef1446bb945965bfcd
YuriiTrofimenko <tyaa@ukr.net>
2021-11-11 13:12:26 +0200

    regex matcher n split

--- src/org/tyaa/demo/java/se/basics/Main.java
+++ src/org/tyaa/demo/java/se/basics/Main.java
@@ -1,7 +1,10 @@
 package org.tyaa.demo.java.se.basics;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -35,15 +38,60 @@ public class Main {
         // String rule = "^[a-z0-9-]{1,255}@[a-z0-9-]{1,25}(.[a-z]{2,3}){1,2}$";
         String rule = "^[a-z0-9-]{1,255}@[a-z0-9-]{1,25}([\\.][a-z]{2,3}){1,2}$";
         // System.out.println(s1.matches("^e[a-z0-9;]{1,}"));
-        System.out.println(s1.matches(rule));
+        /* System.out.println(s1.matches(rule));
         System.out.println(s2.matches(rule));
         System.out.println(s3.matches(rule));
         System.out.println(s4.matches(rule));
         System.out.println(s5.matches(rule));
         System.out.println(s6.matches(rule));
-        System.out.println(s7.matches(rule));
+        System.out.println(s7.matches(rule)); */
 
         // List.of(1, 8, 0).sort(Integer::compareTo);
         // Collections.sort(List.of(1, 8, 0));
+        // rule_1_OfAnnotation = "^+380[0-9]{8,9}";
+        // String rule_2_OfAnnotation = "^+372[0-9]{8,9}";
+
+        /* System.out.println("+33123456789".matches("^\\+380[0-9]{8,9}"));
+        System.out.println("+38023456789".matches("^\\+380[0-9]{8,9}"));
+
+        Pattern phonePattern = Pattern.compile("^\\+380[0-9]{8,9}");
+        System.out.println(phonePattern.matcher("+33123456789").find());
+        System.out.println(phonePattern.matcher("+38023456789").find()); */
+
+        // String pattern = "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        /* String pattern = "\\d{10}|(\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        System.out.println("1234567890".matches(pattern));
+        System.out.println("123-456-7890".matches(pattern));
+        System.out.println("(123)456-7890".matches(pattern));
+        System.out.println("(123)4567890".matches(pattern)); */
+        //\\d{10} matches 1234567890
+        //(?:\d{3}-){2}\d{4} matches 123-456-7890
+        //\(\d{3}\)\d{3}-?\d{4} matches (123)456-7890 or (123)4567890
+
+        String url = "https://localhost:8080";
+        String url2 = "https://localhost:8080/about";
+        String url3 = "https://localhost:8080/items/?orderBy=date&sortingDirection=DESC";
+        String url4 = "https://localhost:8080/items/101";
+        String url5 = "https://localhost:8080/items/?q=orderBy:date;sortingDirection:DESC;price<:1000;categories:[1,3]";
+        /* String[] results = url3.split("\\?");
+        results = results[1].split("&");
+        // System.out.println();
+        Map<String, String> params = new HashMap<>();
+        for (String s : results) {
+            // System.out.println(s);
+            params.put(s.split("\\=")[0], s.split("\\=")[1]);
+        }
+        params.forEach((key, value) -> System.out.printf("%s -> %s\n", key, value)); */
+
+        /*
+        * Pattern pattern = Pattern.compile("([\\w]+?)(:|<|>|<:|>:)([\\w\\]\\[\\,]+?);");
+            Matcher matcher = pattern.matcher(searchModel.searchString + ";");
+            while (matcher.find()) {
+                builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+                * // TODO собрать все результаты в ArrayList, каждым элементом которого будет
+                *     // массив из трех элементов: имя суб-параметра, оператор сравнения и значение,
+                * // и вывести все данные списка в консоль
+            }
+        *  */
     }
 }
